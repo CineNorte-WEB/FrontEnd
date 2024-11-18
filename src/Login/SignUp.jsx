2c0491e858e696023d7a92454643482ec6af4439
Jeon Yong Hak <charismayoung1993@gmail.com>
2024-11-18 10:57:56 +0900

    로그인 유효성 검증 완료 및 기능 추가

--- src/Login/SignUp.jsx
+++ src/Login/SignUp.jsx
@@ -5,10 +5,22 @@ import { FaUserGraduate } from "react-icons/fa";
 import { PiFlowerLotusLight } from "react-icons/pi";
 import { IoMdEyeOff } from "react-icons/io";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState(false);
+  // useForm 훅 초기화
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  // 폼 제출 시 실행될 함수
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("로그인 성공!");
+  };
   const showPassword = () => {
     setPassword((prev) => !prev);
   };
@@ -57,7 +69,7 @@ function SignUp() {
           <PiFlowerLotusLight className="mt-3 text-white text-8xl" />
           <PiFlowerLotusLight className="mt-3 text-white text-8xl" />
         </div>
-        <h1 className="text-white ml-36 fon2t-normal text-8xl font-petemoss">
+        <h1 className="font-normal text-white ml-36 text-8xl font-petemoss">
           CamChelin
         </h1>
       </div>
@@ -65,29 +77,59 @@ function SignUp() {
         <h1 className="mt-3 mb-5 font-normal text-center text-8xl font-petemoss">
           CamChelin
         </h1>
-        <form className="mt-6 space-y-4 ">
+        {/* 리액트 훅 폼을 이용하여 제출하는 폼 요소 시작 부분 */}
+        <form className="mt-6 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          {/* 아이디(이메일 주소)필드 */}
           <p className="ml-3 font-bold font-yeonsung">아이디</p>
           <div className="relative">
             <MdOutlinePermIdentity className="absolute mx-3 mt-3 text-2xl" />
             <input
               id="email"
               type="email"
-              required
+              {...register("name", {
+                required: "올바른 이메일 주소를 입력하세요.",
+                pattern: {
+                  value: /^[a-zA-Z0-9_%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message:
+                    "올바르지 않은 이메일 주소입니다. 다시 입력해주세요!",
+                },
+              })}
               placeholder="아이디(이메일 주소)"
               className="block w-full px-10 py-3 mt-1 border border-black rounded-md shadow-md font-yeonsung mpt focus:border-indigo-500 focus:ring-indigo-500"
             />
+            {/* 에러 메시지 출력 */}
+            {errors.name && <p>{errors.email.message}</p>}
           </div>
           <div>
             <div className="relative mb-7">
               <p className="mb-3 ml-2 font-bold font-yeonsung">비밀번호</p>
               <IoIosLock className="absolute mt-3 ml-3 text-2xl" />
+              {/* 리액트 훅 폼을 이용한 비밀번호 유효성 검증 부분 */}
               <input
                 id="password"
                 type={password ? "text" : "password"}
-                required
+                {...register("password", {
+                  required: "비밀번호를 입력해주세요!",
+                  minLength: {
+                    value: 8,
+                    message: "비밀번호는 최소 8자리 이상이어야 합니다.",
+                  },
+                  validate: {
+                    hasUppercase: (value) =>
+                      /[A-Z]/.test(value) ||
+                      "Must include at least one uppercase letter",
+                    hasNumber: (value) =>
+                      /[0-9]/.test(value) || "Must include at least one number",
+                    hasSpecialChar: (value) =>
+                      /[!@#$%^&*]/.test(value) ||
+                      "Must include at least one special character",
+                  },
+                })}
                 placeholder="비밀번호"
                 className="block w-full px-10 py-3 mt-1 border border-black rounded-md shadow-md m font-yeonsung focus:border-indigo-500 focus:ring-indigo-500"
               />
+              {errors.password && <p>{errors.password.message}</p>}
+
               {password ? (
                 <IoMdEyeOff
                   onClick={showPassword}
@@ -103,7 +145,7 @@ function SignUp() {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-2xl font-bold text-white border border-white rounded-md shadow-lg font-yeonsung bg-rose-500 bg-buttonPink hover:bg-pink-400 focus:outline focus:ring focus:ring-offset-2 focus:ring-pink-500"
+            className="w-full px-4 py-2 text-2xl font-bold text-white border border-white rounded-md shadow-lg font-yeonsung bg-rose-500 hover:bg-pink-400 focus:outline focus:ring focus:ring-offset-2 focus:ring-pink-500"
           >
             로그인
           </button>
