e827555aa0ac2c9375485009b3fddb6311f70995
JeonYongHak <charismayoung1993@gmail.com>
2024-11-25 16:40:51 +0900

    페이지별 선택 시 색깔 및 투명도 변화 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import PassWord from "./Login/PassWord";
 import SignUp from "./Login/SignUp";
 import SignIn from "./Login/SignIn";
 import NotFound from "./components/NotFound";
+import Common from "./components/Common";
 import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <Routes>
+      <Common />
+      {/* <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/password" element={<PassWord />} />
         {/* 위 3개의 페이지가 아닐 시에 *로 표시된 페이지가 렌더링됨 */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      {/* <Route path="*" element={<NotFound />} /> */}
+      {/* </Routes> */}
     </>
   );
 };
